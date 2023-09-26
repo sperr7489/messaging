d32016a367ec33882cb24e2e478820fb72a10dec
김기창 <sperr@ajou.ac.kr>
2023-09-26 16:55:14 +0900

    log d

--- src/crawler/crawler.service.ts
+++ src/crawler/crawler.service.ts
@@ -169,7 +169,6 @@ export class CrawlerService {
                 reservationNumber,
                 price,
               );
-              reservation.displayInfo();
               let place = places.find(
                 (place) => place.description == placeReservation,
               );
